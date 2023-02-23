7ada388ed49097d52676185ddfd8229947313fd5
Aditya Kushwaha <kaditya9711@gmail.com>
2023-02-23 07:38:49 +0545

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+```sh
+yarn create vite vite-test --template react-ts
+```
+```sh
+yarn
+```
+```sh
+yarn add -D tailwindcss postcss autoprefixer
+```
+```sh
+npx tailwindcss init -p
+```
+```sh
+yarn run dev
+```
---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+
+  return (
+    <div className="ak-flex ak-m-10 ak-justify-center dark:ak-bg-black dark:ak-text-white">
+      Aditya is making these all changes
+    </div>
+  )
+}
+
+export default App
